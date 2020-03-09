7cbb95436b923d27c6a995bdc2aa27f748373be3
IshaanDesai45 <46575231+IshaanDesai45@users.noreply.github.com>
2020-03-10 02:38:46 +0530

    webpack config updated
    @babel/plugin-proposal-class-properties added

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,31 @@ var path = require('path');
 var HWP = require('html-webpack-plugin');
 
 module.exports ={
+    
     entry:'./src/index.js',
     output:{
         path:path.join(__dirname,'/dist'),
-        filename:'index_bundle.js'
+        filename:'index_bundle.js',
+        publicPath:'/'
     },
+    
     module: {
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"babel-loader"
-                }
-            }
-        ]
-    },
+        rules: [
+           {
+              test: /\.js$/,
+              use: 'babel-loader',
+           },
+           {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader'],
+           },
+           
+  ]
+     },
     plugins: [
         new HWP({
-            template:'./public/index.html'
+            template:path.join(__dirname,'public/index.html'),
+            filename:'index.html'
         }),
         new webpack.HotModuleReplacementPlugin(),
         // new webpack.NoErrorsPlugin()
